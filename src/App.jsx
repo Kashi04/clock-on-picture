ed6d10b701f798199eae859d5a6f5d680d41ecce
Muhammad Kashif <muhammadkashif8078@gmail.com>
2024-05-18 20:41:41 +0500

    complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DigitalClock from "./DigitalClock";
+import "./index.css";
+import StopWatch from "./stop-watch/StopWatch";
+
+function App() {
+  return (
+    <div>
+      <DigitalClock />
+      {/* <StopWatch /> */}
+    </div>
+  );
+}
+
+export default App;
